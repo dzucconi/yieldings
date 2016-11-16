0ee4e53e06def89c09f433fcb4d7955a9d73d23c
dzucconi <mail@damonzucconi.com>
2016-11-16 13:20:52 +0100

    Initial commit

---
+++ app/javascripts/index.js
@@ -0,0 +1,56 @@
+import { diffChars } from 'diff';
+
+const STATE = {
+  editor: '',
+  removed: '',
+};
+
+const DOM = {
+  app: document.getElementById('app'),
+  editor: document.getElementById('editor'),
+  removed: document.getElementById('removed'),
+  reset: document.getElementById('reset'),
+};
+
+export default () => {
+  try {
+    STATE.editor = DOM.editor.value = localStorage.getItem('editor');
+    STATE.removed = DOM.removed.value = localStorage.getItem('removed');
+  } catch(e) {
+    // Ignore
+  }
+
+  STATE.editor = DOM.editor.value;
+
+  DOM.editor.addEventListener('input', () => {
+    const diff = diffChars(STATE.editor, DOM.editor.value);
+
+    diff.forEach(part => {
+      if (part.removed) {
+        STATE.removed += part.value;
+      }
+    });
+
+    try {
+      localStorage.setItem('editor', DOM.editor.value);
+      localStorage.setItem('removed', DOM.removed.value = STATE.removed);
+    } catch(e) {
+      // Ignore
+    }
+
+    STATE.editor = DOM.editor.value;
+  });
+
+  DOM.reset.addEventListener('click', e => {
+    e.preventDefault();
+
+    try {
+      localStorage.setItem('editor', STATE.editor = DOM.editor.value = '');
+      localStorage.setItem('removed', STATE.removed = DOM.removed.value = '');
+    } catch(e) {
+      // Ignore
+    }
+
+    DOM.editor.focus();
+  });
+};
