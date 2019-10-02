97bc68f174f4b6fdba6350c0275b2e7c0e5c1953
dzucconi <mail@damonzucconi.com>
2019-10-01 21:52:14 -0400

    Improves audio

--- src/audio/index.ts
+++ src/audio/index.ts
@@ -7,7 +7,7 @@ import space from "./space.mp3";
 import backspace from "./backspace.mp3";
 
 export const audio = {
-  type: new Howl({ src: [type], autoplay: false }),
-  space: new Howl({ src: [space], autoplay: false }),
-  backspace: new Howl({ src: [backspace], autoplay: false })
+  type: new Howl({ src: [type], autoplay: false, preload: true }),
+  space: new Howl({ src: [space], autoplay: false, preload: true }),
+  backspace: new Howl({ src: [backspace], autoplay: false, preload: true })
 };
