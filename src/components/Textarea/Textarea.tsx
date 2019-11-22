0117195f01ba347360a550f45e482c5f6ff4afe8
dzucconi <mail@damonzucconi.com>
2019-11-22 07:36:06 -0500

    Neutralizes margins in Safari

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Textarea = styled.textarea`
-  font-size: 2rem;
+  margin: 0;
   padding: 0.5em 0.75em;
+  font-size: 2rem;
   color: currentColor;
 `;
